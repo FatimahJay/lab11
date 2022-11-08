16af33517a8642f285dd94c1d9c7ba8353a9def9
Fatima <104238604+FatimahJay@users.noreply.github.com>
2022-11-08 15:55:57 +0500

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//task1
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // gets the component by id
+  const linkElement = screen.getByTestId("comp");    
+  expect(linkElement).toBeInTheDocument();   // finds the component in the document if it exists
+});
+
+//task2
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId("comp");
+  expect(linkElement).toHaveTextContent("Hello World");
+
 });
